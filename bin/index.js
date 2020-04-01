a01f9d89eebfe272a761ed661ab8c4a7b667b351
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-03-31 17:57:53 -0700

    Wait for all processes to close

--- bin/index.js
+++ bin/index.js
@@ -6,20 +6,28 @@ const [,, ...args] = process.argv
 
 if (args.length === 0) process.exit()
 
-const commands = args.join(' ').split(/('|")\s/).map( string => string.replace(/'|"/g, '') ).filter(Boolean)
+const commands = args.join(' ').split(/\s?\+\s?/).filter(Boolean)
+
+console.log(args.join(' '), commands)
+
+process.stdin.resume()
+
+let started = 0
 
 for (command of commands) {
+  started++
+
   const child = exec(command, {
     cwd: process.cwd()
   })
 
   child.stdout.pipe(process.stdout, { end: false })
 
-  process.stdin.resume()
-
   process.stdin.pipe(child.stdin, { end: false })
 
   child.stdin.on('end', () => {})
 
-  child.on('exit', process.exit)
+  child.on('exit', code => {
+    if (!--started) process.exit(code)
+  })
 }
